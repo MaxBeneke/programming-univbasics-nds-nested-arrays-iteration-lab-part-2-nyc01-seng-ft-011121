81847e63408d8e3cbee4c30c4cd983dcfa6523e0
Max Beneke <max.beneke@gmail.com>
2020-12-09 13:57:30 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,6 +5,7 @@ def find_min_in_nested_arrays(src)
   
   while row_index < src.count do
     element_index = 0
+    inner_results = []
     minimum_string_element = 100
     
     while element_index < src[row_index].count do
